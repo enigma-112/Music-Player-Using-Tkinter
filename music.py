abf918d4669a76e80d35d2e59d0d52601d64936b
Kundan Kumar <rrd51510@gmail.com>
2019-01-25 22:42:31 +0530

    title and icon added

--- music.py
+++ music.py
@@ -1,4 +1,13 @@
 from tkinter import *
 root = Tk()
 
+root.title("Galactic Music Player")
+#root.iconbitmap('@m_player.xbm')
+	
+if ( sys.platform.startswith('win')): 
+	root.iconbitmap('m_player.ico')
+else:
+	logo = PhotoImage(file='m_player.gif')
+	root.call('wm', 'iconphoto', root._w, logo)
+
 root.mainloop()
